333c33c4f51f85eb4ad8817718a8482671672004
Yuelin Zhao <yuelinzhao@Yuelins-iMac-23.lan>
2023-04-12 00:01:25 -0400

    remvoe config.php

--- colincms/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-define('HOSTNAME', 'http://api.wangzubeauty.com');
-
-define('RULES', array(
-	['prefix'=>'wangzu','id'=>'10','siteurl'=>'http://www.wangzubeauty.com'],
-	['prefix'=>'sample','id'=>'11','siteurl'=>'http://www.sample.com']
-));
